d2a51e506452858b10aab30ef6c745a0cfc0cd27
Jason Housley <housleyjk@gmail.com>
2014-08-06 15:03:01 -0600

    UWSGI websocket testing.

--- aiopyramid/websocket/config.py
+++ aiopyramid/websocket/config.py
@@ -3,6 +3,7 @@ import asyncio
 
 import greenlet
 from aiopyramid.config import AsyncioMapperBase
+from aiopyramid.helpers import run_in_greenlet
 
 try:
     import uwsgi
@@ -18,12 +19,15 @@ def uwsgi_recv_msg(g):
 class UWSGIWebsocket:
 
     def __init__(self, back, q_in, q_out):
+        print('initializing socket')
         self.back = back
         self.q_in = q_in
         self.q_out = q_out
 
     @asyncio.coroutine
     def recv(self):
+        print('in receive')
+        print(self.q_in.qsize())
         return (yield from self.q_in.get())
 
     @asyncio.coroutine
@@ -56,11 +60,15 @@ class UWSGIWebsocketMapper(AsyncioMapperBase):
             # wait for pingback
             this.parent.switch()
 
+            future = asyncio.Future()
+            asyncio.async(run_in_greenlet(this, future, view(context, request)))
+
             while True:
                 if this.has_message:
                     this.has_message = False
                     msg = uwsgi.websocket_recv_nb()
-                    q_in.put_nowait(msg)
+                    if msg or msg is None:
+                        q_in.put_nowait(msg)
                     if msg is None:
                         break
                 if not q_out.empty():
@@ -72,15 +80,5 @@ class UWSGIWebsocketMapper(AsyncioMapperBase):
         return websocket_view
 
     def __call__(self, view):
-        print('calling', view)
-        #if not asyncio.iscoroutinefunction(view) and is_generator(view):
-            #view = asyncio.coroutine(view)
-        #else:
-            #raise ConfigurationError(
-                #'Non-coroutine {} mapped to coroutine.'.format(view)
-            #)
-
-        view = super().__call__(view)
-        wrapped = self.launch_websocket_view(view)
-        print(wrapped)
-        return wrapped
+        """ Accepts a view_callable class. """
+        return self.launch_websocket_view(view)
