8aef5f4f8c823d138f36b68df2ad40a8b5adb6a3
Jason Housley <HousleyJK@gmail.com>
2016-02-18 18:04:35 -0700

    Fix pep8 conformance

--- aiopyramid/gunicorn/worker.py
+++ aiopyramid/gunicorn/worker.py
@@ -37,7 +37,7 @@ def atoms(resp, req, environ, request_time):
         'f': environ.get('HTTP_REFERER', '-'),
         'a': environ.get('HTTP_USER_AGENT', '-'),
         'T': request_time.seconds,
-        'D': (request_time.seconds*1000000) + request_time.microseconds,
+        'D': (request_time.seconds * 1000000) + request_time.microseconds,
         'L': "%d.%06d" % (request_time.seconds, request_time.microseconds),
         'p': "<%s>" % os.getpid()
     }
@@ -52,7 +52,8 @@ def atoms(resp, req, environ, request_time):
         dict([("{%s}i" % k.lower(), v) for k, v in req_headers.items()]))
 
     # add response headers
-    atoms.update(dict([("{%s}o" % k.lower(), v) for k, v in resp.headers.items()]))
+    atoms.update(dict(
+        [("{%s}o" % k.lower(), v) for k, v in resp.headers.items()]))
 
     return atoms
 
@@ -111,7 +112,8 @@ class AiopyramidHttpServerProtocol(WSGIServerHttpProtocol):
             environ,
             datetime.timedelta(0, 0, time))
         safe_atoms = self.logger.atoms_wrapper_class(catoms)
-        self.logger.access_log.info(self.logger.cfg.access_log_format % safe_atoms)
+        self.logger.access_log.info(
+            self.logger.cfg.access_log_format % safe_atoms)
 
 
 class AsyncGunicornWorker(AiohttpWorker):
